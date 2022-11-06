3341f71d49b930bbed6266c0e45373de3396b68f
Moulik Rai <raimoulik@gmail.com>
2022-11-06 15:06:27 +0545

    chore: add clone deep function

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-const a = "asdf";
+/**
+ *   Clone utility function
+ *   @param {Any} input - Initial product price
+ *   @return {Any} Price of the product
+ **/
+
+export const cloneDeep = <T>(input: T): T => {
+  if (Array.isArray(input)) {
+    const clonedArray = input.map((item) => cloneDeep(item))
+
+    return clonedArray as T
+  }
+
+  if (typeof input === 'object') {
+    for (const key in input) {
+      input[key] = cloneDeep(input[key])
+    }
+
+    return { ...input }
+  }
+
+  return input
+}
